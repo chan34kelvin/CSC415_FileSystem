eef538177ae16a8f26a80a70c4b98a365acf1b5d
Kelvin Chan <31221043+chan34kelvin@users.noreply.github.com>
2020-04-23 00:19:06 -0700

    Update fsDriver3.c

--- fsDriver3.c
+++ fsDriver3.c
@@ -1,45 +1,66 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include "fsLow.h"
+#include "FileSystemInfo.h"
+#include "initializeFileSystem.c"
+#include "closeFileSystem.c"
+
 int main(int argc, char *argv[]){
+	//vars inside main and initialize
+	char *filename= (argc>0)? argv[1]:"";
+	uint64_t volumeSize= (argc>1)? atoll(argv[2]):0;
+	uint64_t blockSize= (argc>2)? atoll(argv[3]):0;
 
-	char *fileName;
-	long int volSize;
-	long int blockSize;
+	//start partition 
+	startPartitionSystem (filename, &volumeSize, &blockSize);
 
-	if(argc>3){
-		fileName= argv[1];
-		volSize= atoll(argv[2]);
-		blockSize=atoll(argv[3]);
+	//initialize all variables use in RAM
+	initializeFileSystem(blockSize);	
+	//printf("test: %d\n",fileEntries[2].id);
+	//test
+	/*for(int i=0;i<50;i++){
+		char name[51]="test";
+		//strcat(name,atoll(i));
+		fileEntries[i].id=i;
+		dirEntries[i].id=i;
+		strcpy(fileEntries[i].fileName, name);
+		strcpy(fileEntries[i].content, "OOOOOO");
+		strcpy(dirEntries[i].dirName, name);
+		strcpy(dirEntries[i].parentDir, "OOOOOO");
 	}
+	numOfDirsRAM=50;
+	numOfFilesRAM=50;*/
 
-	int retVal= startPartitionSystem(fileName, &volSize, &blockSize);
+	//close file system
+	closingFileSystem(blockSize);
 
-	if(retVal!=0){
-		printf("File system terminated");
-		return 0;
-	}
-
-	/*printf("Please enter a user file name: ");
-	char userFileName[128];
-	scanf("%s",userFileName);*/
-
-	DirectoryEntryP name[10000];
-	DirectoryEntry C;
-	C.size= 800;
-	long int count= sizeof(DirectoryEntryP);
-	DirectoryEntryP C1= malloc(blockSize );
-	C1= &C;
-	printf("%d",sizeof(DirectoryEntryP));	
-	LBAwrite (C1, 1,0);
-	DirectoryEntryP C2= malloc(blockSize );
-	LBAread (C2, 1,0);
-	DirectoryEntry D;
-	D.size=10;
-	DirectoryEntryP D1= malloc(blockSize );
-	D1= &D;
-	LBAwrite (D1, 2,1);
-	DirectoryEntryP D2= malloc(blockSize );
-	LBAread (D2, 2, 1);
-	printf("%d",D2->size);
-	printf("%d",C2->size);
+	//close partition
+	freeMemory();
 	closePartitionSystem();
 	return 0;
-  }
+}
+
+//free all malloc
+void freeMemory(){
+	free(memory);
+	free(memEnd);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
