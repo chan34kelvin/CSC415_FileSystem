aa95cf9789f539daefc8ee834e58ccf2fdd79d85
Kelvin Chan <31221043+chan34kelvin@users.noreply.github.com>
2020-04-26 20:16:30 -0700

    Create fsDriver3.c

---
+++ fsDriver3.c
@@ -0,0 +1,64 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include "fsLow.h"
+#include "FileSystemInfo.h"
+#include "initializeFileSystem.c"
+#include "closeFileSystem.c"
+#include "FileSystemControl.c"
+#include "checkFunctionsP1.c"
+#include "driverFunctionP1.c"
+#include "checkFunctions.c"
+#include "driverFunctionP2.c"
+#include "driverFunctionP3.c"
+#include "driverFunctionP4.c"
+
+int main(int argc, char *argv[]){
+	//vars inside main and initialize
+	char *filename= (argc>0)? argv[1]:"";
+	uint64_t volumeSize= (argc>1)? atoll(argv[2]):0;
+	uint64_t blockSize= (argc>2)? atoll(argv[3]):0;
+
+	//start partition 
+	startPartitionSystem (filename, &volumeSize, &blockSize);
+
+	//initialize all variables use in RAM
+	initializeFileSystem(blockSize);
+
+	/*for(int i=1;i<=10;i++){
+		printf("at index %d :%d|",i,freeEntriesDir[i]);
+	}*/
+	printf("Number of free entries atm1 %d\n",numOfFreeDirs);
+	//make a ROOT directory
+	if(numOfDirsRAM==0){
+		cdLoc=numOfDirsRAM;
+		creatingDir(nullVal,"ROOT",nullArrDirs,nullArrFiles,0);
+	}
+
+	//call control center
+	controlCenter(blockSize,volumeSize,filename);
+
+	//close file system
+	closingFileSystem(blockSize);
+
+	//close partition
+	freeMemory();
+	closePartitionSystem();
+	return 0;
+}
+
+//free all malloc
+void freeMemory(){
+	free(memory);
+	free(memEnd);
+}
+
+
+
+
+
